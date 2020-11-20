99ed76e03f10996111c715483f8962e34f5576d1
su787910081 <su_787910081@sina.com>
2020-11-20 14:17:00 +0800

    Create TaskService.java
    1

---
+++ TaskService.java
@@ -0,0 +1,47 @@
+package com.suyh.async.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+/**
+ * @author 苏雲弘
+ * @since 2020-11-20
+ */
+@Slf4j
+@Component
+public class TaskService {
+
+    public static Random random = new Random();
+
+    // Async 注解的参数指定了线程池的bean 名称，可以指定在哪个线程池bean 里面执行。
+    @Async("taskExecutor")
+    public void doTaskOne() throws Exception {
+        log.info("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        log.info("完成任务一，耗时：" + (end - start) + "毫秒");
+    }
+
+    @Async("taskExecutor02")
+    public void doTaskTwo() throws Exception {
+        log.info("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        log.info("完成任务二，耗时：" + (end - start) + "毫秒");
+    }
+
+    @Async
+    public void doTaskThree() throws Exception {
+        log.info("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        log.info("完成任务三，耗时：" + (end - start) + "毫秒");
+    }
+
+}
